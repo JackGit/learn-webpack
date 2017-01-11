151d052e8689e3b36535b949175a3495c0b23d37
Jack Yang <ygjack414@hotmail.com>
2017-01-11 21:35:54 +0800

    update htmlwebpackplugin configs

--- 04.HtmlWebpackPlugin/webpack.config.js
+++ 04.HtmlWebpackPlugin/webpack.config.js
@@ -3,7 +3,13 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var HTMLWebpackPlugin = require('html-webpack-plugin')
 var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin
 
-
+/**
+ * HTMLWebpackPlugin是将指定chunk的JS和extract出来的CSS文件自动插入HTML模板的插件，是第三方的，非webpack官方插件
+ *
+ * 例如： new HTMLWebpackPlugin() 则默认将所有chunk对应的JS和CSS插入
+ * 例如： new HTMLWebpackPlugin({chunks: ['page-a']}) 则指定将chunk page-a相关的JS和CSS插入
+ * 例如： new HTMLWebpackPlugin({excludeChunks: ['page-b']})  则可排除page-b相关
+ */
 module.exports = {
 
   // will create 2 chunks and 1 html file
@@ -76,8 +82,8 @@ module.exports = {
 
 
   // will have 2 chunks, 2 css files and 2 html
-  // and the 2 html file has the same content, which includes all the chunks and css files
-  entry: {
+  // and the 2 html file has the same content, they all include all the 2 chunks and 2 css files
+  /*entry: {
     'page-a': './pageA.js',
     'page-b': './pageB.js'
   },
@@ -99,8 +105,32 @@ module.exports = {
     new HTMLWebpackPlugin({
       filename: 'page-b.html'
     })
-  ]
+  ]*/
 
   // how to create page-a.html includes page-a.js and page-a.css, and create another page-b.html, includes page-b.js and page-b.css?
-  
+  entry: {
+    'page-a': './pageA.js',
+    'page-b': './pageB.js'
+  },
+  output: {
+    path: './output',
+    filename: 'output-[name].js'
+  },
+  module: {
+    loaders: [{
+      test: /\.css$/,
+      loader: ExtractTextPlugin.extract('css-loader')
+    }]
+  },
+  plugins: [
+    new ExtractTextPlugin('extract-[name].css'),
+    new HTMLWebpackPlugin({
+      filename: 'page-a.html',
+      chunks: ['page-a']  // this let HTMLWebpackPlugin know only to pack chunk page-a related js and css file to the html
+    }),
+    new HTMLWebpackPlugin({
+      filename: 'page-b.html',
+      chunks: ['page-b']  // this let HTMLWebpackPlugin know only to pack chunk page-b related js and css file to the html
+    })
+  ]
 }
